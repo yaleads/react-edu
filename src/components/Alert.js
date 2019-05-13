a6f28b7deef82025439f08637895786617ee39c7
artemiarmilko <yarm@mail.ru>
2019-05-13 12:21:13 +0300

    Alert fix

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,14 +1,12 @@
 import React from 'react';
+import { UncontrolledAlert } from 'reactstrap';
 
 const Alert = ({type = 'danger', text = undefined}) => {
   if (text !== undefined) {
     return (
-      <div className={`alert alert-${type} alert-dismissible fade show mt-3`} role="alert">
-        <strong>Error!</strong> {text}
-        <button type="button" className="close" data-dismiss="alert" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
+      <UncontrolledAlert color={type}>
+        {text}
+      </UncontrolledAlert>
     )
   }
   else return null;
