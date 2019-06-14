e9c7751e46b7f8e9dd58bdf3ef00470688267cff
artemiarmilko <yarm@mail.ru>
2019-06-14 14:58:20 -0700

    pm2

--- pm2.config.js
+++ pm2.config.js
@@ -9,5 +9,16 @@ module.exports = {
     env_production: {
       NODE_ENV: 'production'
     }
-  }]
+  }],
+
+  deploy: {
+    production: {
+      user: 'pm2',
+      host: ['138.68.234.157'],
+      ref: 'origin/lesson09',
+      repo: 'https://github.com/yaleads/react-edu.git',
+      path: '/home/pm2/app',
+      'post-deploy': 'yarn && yarn build:server && pm2 startOnRestart pm2.config.js --env production'
+    }
+  }
 };
